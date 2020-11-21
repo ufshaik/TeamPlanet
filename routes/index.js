bf838e38f865b15303aa81c82d8d1a012a6b596a
Umar Shaik <umar@creatros.ca>
2020-11-21 17:17:31 -0330

    changes 3.0

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,19 @@ module.exports = function(app) {
         res.render('pages/index');
     });
 
+    app.get('/buttons', function(req, res) {
+        res.render('pages/buttons');
+    });
+
+    app.get('/tables', function(req, res) {
+        res.render('pages/tables');
+    });
+
+    app.get('/charts', function(req, res) {
+        res.render('pages/charts');
+    });
+
+
     app.get('/about', function(req, res) {
         res.render('pages/about');
     });
